e333529451de7f5208ad44db9cea14aef64e3d9a
miguelvelemj25 <miguelvelez@mijecu25.com>
2015-04-28 10:44:03 -0500

    Initial commit to working version of 1.1

--- java/chapter1/1.1/question.java
+++ java/chapter1/1.1/question.java
@@ -6,7 +6,36 @@
 public class question {
 
 	public static void main(String[] args) {
-		System.out.println("Testing");
+		System.out.println(question.uniqueCharacters("miguel") + "\n"); // True
+		System.out.println(question.uniqueCharacters("miguelguel")); // False
+	}
+
+	/**
+	 * Check if the string contains unique characters 
+	 */
+	public static boolean uniqueCharacters(String string) {
+		// Array for all possible ASCII characters
+		int[] characters = new int[128];
+
+		// Loop through the characters of the string
+		for(int i = 0; i < string.length(); i++) {
+			// Get the current character
+			int character = string.charAt(i);
+			System.out.println(character + " - " + string.charAt(i));
+
+			// Search if it is in the array
+			if(characters[character] == 1) {
+				// It means that the character has been seen
+				return false;
+			}
+
+			// The character has not been seen, so add set in the array that we have seen it
+			characters[character] = 1;
+		}
+
+		// If we looped through all the characters without returning, it means that all of
+		// them are unique
+		return true;
 	}
 
 }
