7f71cbcbd9f5ad90d004d5490371568b0f17605b
miguelvelemj25 <miguelvelez@mijecu25.com>
2015-04-28 10:46:07 -0500

    Initial commit of 1.2

---
+++ java/chapter1/1.2/question.java
@@ -0,0 +1,12 @@
+/*
+	Implement a function void reversefchar* str) in Cor C++ which reverses a null-termi- nated string.
+*/
+
+public class question {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
+	
