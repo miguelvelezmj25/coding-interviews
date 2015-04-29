17fe2f8046c33ae2e2a2bf36354f2c930c300b8b
Miguel Velez <miguelvelez@mijecu25.com>
2015-04-29 07:31:26 -0500

    Figured out something important

--- java/chapter1/1.2/question.java
+++ java/chapter1/1.2/question.java
@@ -2,6 +2,7 @@
 	Implement a function void reversefchar* str) in Cor C++ which reverses a null-termi- nated string.
 */
 
+// TODO this question cannot be implemented in java since java strings are immutable
 public class question {
 
 	public static void main(String[] args) {
