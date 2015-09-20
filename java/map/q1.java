f219d7db316463f0c655875ae3db2c0f29b87003
miguelvelezmj25 <miguelvelez@mijecu25.com>
2015-09-20 11:18:51 -0500

    Done with q1

---
+++ java/map/q1.java
@@ -0,0 +1,39 @@
+// You are given a phone book that consists of people's names and their phone number. After that 
+// you will be given some person's name as query. For each query, print the phone number of that 
+// person.
+
+import java.util.*;
+import java.io.*;
+
+public class Solution {	
+
+	public static void main(String []argh) {
+		Map<String, Integer> phonebook  = new HashMap<String, Integer>();
+		
+		Scanner in = new Scanner(System.in);
+		int n=in.nextInt();
+		in.nextLine();
+
+		for(int i=0;i<n;i++) {
+			String name=in.nextLine();
+		        int phone=in.nextInt();
+
+			phonebook.put(name, phone);
+
+			in.nextLine();
+		}
+
+		while(in.hasNext()) {
+			String s=in.nextLine();
+
+			if(phonebook.containsKey(s)) {
+				System.out.println(s + "=" + phonebook.get(s));
+			}
+			else {
+				System.out.println("Not found");
+			}
+		}
+		
+	}
+}
+
